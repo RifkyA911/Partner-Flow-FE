8118b9f9dc9bb3ca6462caeeff8fedd52bcdf02c
Rifky Akhmad F <38658854+RifkyA911@users.noreply.github.com>
2026-06-04 20:43:15 +0700

    chore: add env example and app config

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL || process.env.BACKEND_URL || 'http://localhost:8080';
+export const googleClientId = process.env.GOOGLE_CLIENT_ID || '';
+export const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET || '';
